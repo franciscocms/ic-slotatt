f573827333dda22ade768f6b71b50e3be375024e
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2024-09-30 19:08:25 +0100

    clevr commits

---
+++ inference/train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min8gb   # Debug partition
+#SBATCH --qos=gpu_min8gb         # Debug QoS level
+#SBATCH --job-name=icsa         # Job name
+#SBATCH -o slurm.%N.%j.out       # File containing STDOUT output
+#SBATCH -e slurm.%N.%j.err       # File containing STDERR output
+
+python train.py
