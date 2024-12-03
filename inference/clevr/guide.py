f9be82f06628a5766e22c9368f13f40ff69c9b72
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2024-12-03 17:46:30 +0000

    Initial commit

--- inference/clevr/guide.py
+++ inference/clevr/guide.py
@@ -287,7 +287,7 @@ class InvSlotAttentionGuide(nn.Module):
     
     return out
   
-  def forward(self, observations={"image": torch.zeros((1, 3, 128, 128))}):
+  def forward(self, observations={"image": torch.zeros((1, 3, 128, 128))}, objects_mask=torch.bernoulli(torch.zeros(1, 2))):
 
     # register networks to be optimized
     pyro.module("encoder_sa", self.encoder_cnn.encoder_sa, True)
