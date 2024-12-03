bc3bd1be399c39b6104c5111167043e5774100f3
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2024-12-03 18:12:53 +0000

    Initial commit

--- main/modifiedCSIS.py
+++ main/modifiedCSIS.py
@@ -148,7 +148,7 @@ class CSIS(Importance):
       if name not in ["image", "n_plate"] and vals["type"] == "sample" and name.split('_')[0] not in hidden_addr: 
         #if name == "N": self.n_objects = to_int(vals["value"])
 
-        logger.info(f"{name} - {vals['value']}")
+        logger.info(f"{name} - {vals['value']} - {vals['fn']}")
         
         # prior categorical distributed variables
         if isinstance(vals["fn"], CategoricalVals) or isinstance(vals["fn"], dist.Categorical): 
