f0c8bfc45dbcf56f9d85d61d0de72d9811dfff98
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-01-24 16:22:17 +0000

    Initial commit

--- inference/clevr/eval.py
+++ inference/clevr/eval.py
@@ -101,7 +101,7 @@ def main():
                                     stage = "eval"
                                     ).to(device)
         
-        GUIDE_PATH = f"{main_dir}/checkpoint-{params['jobID']}/guide_{params['guide_step']}.pth"
+        GUIDE_PATH = os.path.join(main_dir, "inference", f"checkpoint-{params['jobID']}", f"guide_{params['guide_step']}.pth")
         if os.path.isfile(GUIDE_PATH): guide = load_trained_guide_clevr(guide, GUIDE_PATH, 
                                                                         dict(mat_map=material_mapping,
                                                                              shape_map=object_mapping,
