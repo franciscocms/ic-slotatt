39d70b2790e4389aa1b2347d228d80ad161e7e4c
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-01-27 08:21:29 +0000

    Initial commit

--- main/clevr_model.py
+++ main/clevr_model.py
@@ -566,8 +566,11 @@ def clevr_gen_model(observations={"image": torch.zeros((1, 3, 128, 128))}):
         
         # stddev = 0.01  in jobID 78
         # stddev = 0.001 in jobID 79
+
+        if params['stage'] == 'train': llh_uncertainty = 0.001
+        elif params['stage'] == 'eval': llh_uncertainty = 0.1
         
-        likelihood_fn = MyNormal(proc_img, torch.tensor(0.001)).get_dist() 
+        likelihood_fn = MyNormal(proc_img, torch.tensor(llh_uncertainty)).get_dist() 
         pyro.sample("image", likelihood_fn.to_event(3), obs=observations["image"])
     
     batch_time = time.time() - init_time
