b058d96c39f767b7b32cb5fe8a824417df48ebbe
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-01-28 08:17:23 +0000

    Initial commit

--- main/clevr_model.py
+++ main/clevr_model.py
@@ -586,14 +586,14 @@ def clevr_gen_model(observations={"image": torch.zeros((1, 3, 128, 128))}):
 
     logger.info(f"gen image shape: {proc_img.shape}")
 
-    with pyro.plate(observations["image"].shape[0]):
+    #with pyro.plate(observations["image"].shape[0]):
         #pyro.sample("image", MyBernoulli(proc_img, validate_args=False).to_event(3), obs=observations["image"])
         
         # stddev = 0.01  in jobID 78
         # stddev = 0.001 in jobID 79
         
-        likelihood_fn = MyNormal(proc_img, torch.tensor(llh_uncertainty)).get_dist() 
-        pyro.sample("image", likelihood_fn.to_event(3), obs=observations["image"])
+    likelihood_fn = MyNormal(proc_img, torch.tensor(llh_uncertainty)).get_dist() 
+    pyro.sample("image", likelihood_fn.to_event(3), obs=observations["image"])
     
     batch_time = time.time() - init_time
     
