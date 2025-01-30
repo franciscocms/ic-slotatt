ba006b046ffd2a81f3453e431f3122942cf3e2d3
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-01-30 13:47:35 +0000

    Initial commit

--- inference/clevr/eval.sh
+++ inference/clevr/eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min11gb   # Debug partition
-#SBATCH --qos=gpu_min11gb         # Debug QoS level
-#SBATCH --job-name=eval_3        # Job name
+#SBATCH --partition=gpu_min12gb   # Debug partition
+#SBATCH --qos=gpu_min12gb         # Debug QoS level
+#SBATCH --job-name=eval_4        # Job name
 #SBATCH -o slurm.%N.%j.out       # File containing STDOUT output
 #SBATCH -e slurm.%N.%j.err       # File containing STDERR output
 
