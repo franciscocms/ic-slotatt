874dbd5de4826dca4e7b4459cfcb30eb6bf10f5f
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-02-03 10:38:03 +0000

    Initial commit

--- main/models.py
+++ main/models.py
@@ -82,6 +82,9 @@ def model(observations={"image": torch.zeros((1, 3, 128, 128))}, show='all', sav
 
   llh_uncertainty = 0.001 if params['running_type'] == "train" else 0.1
   likelihood_fn = MyNormal(img, torch.tensor(llh_uncertainty)).get_dist()
+  
+  logger.info(likelihood_fn)
+
   pyro.sample("image", likelihood_fn.to_event(3), obs=observations["image"])
 
 
