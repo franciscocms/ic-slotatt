a41611693355ac7201921e871720a9aad78a32b3
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-02-04 16:42:52 +0000

    Initial commit

--- inference/guide.py
+++ inference/guide.py
@@ -264,8 +264,6 @@ class InvSlotAttentionGuide(nn.Module):
       variable_prior_distribution = variable.prior_distribution
       variable_proposal_distribution = variable.proposal_distribution
     
-    logger.info(obs.shape)
-    
 
     if variable_proposal_distribution == "normal":
       proposal = obs.squeeze(-1)
