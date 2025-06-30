185a9baf1b4cd0fd02e859afdc93d6468ab20d6f
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-06-30 17:09:04 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -573,13 +573,13 @@ train_data = CLEVR(images_path = os.path.join(dataset_path, 'images/train'),
                    scenes_path = os.path.join(dataset_path, 'scenes/CLEVR_train_scenes.json'),
                    max_objs=10)
 train_dataloader = DataLoader(train_data, batch_size = params["batch_size"],
-                              shuffle=True, num_workers=4, generator=torch.Generator(device='cuda'))
+                              shuffle=True, generator=torch.Generator(device='cuda'))
 val_images_path = os.path.join(dataset_path, 'images/val')
 val_data = CLEVR(images_path = os.path.join(dataset_path, 'images/val'),
                    scenes_path = os.path.join(dataset_path, 'scenes/CLEVR_val_scenes.json'),
                    max_objs=10)
 val_dataloader = DataLoader(val_data, batch_size = params["batch_size"],
-                              shuffle=False, num_workers=4, generator=torch.Generator(device='cuda'))
+                              shuffle=False, generator=torch.Generator(device='cuda'))
 
 
 trainer = Trainer(guide, {"train": train_dataloader, "validation": val_dataloader}, params, run)
