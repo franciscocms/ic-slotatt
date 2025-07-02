4ffa9e8038c80916769520eacb5f4bd1a01d1929
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-07-02 08:45:13 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -273,7 +273,7 @@ class InvSlotAttentionGuide(nn.Module):
     
     preds = self.mlp_preds(self.slots)
     preds[:, :, 0:3] = self.sigmoid(preds[:, :, 0:3].clone())          # coords
-    preds[:, :, 3:5] = self.softmax(preds[:, :, 3:4].clone())       # size
+    preds[:, :, 3:5] = self.softmax(preds[:, :, 3:5].clone())       # size
     preds[:, :, 5:7] = self.softmax(preds[:, :, 5:7].clone())       # material
     preds[:, :, 7:10] = self.softmax(preds[:, :, 7:10].clone())     # shape
     preds[:, :, 10:18] = self.softmax(preds[:, :, 10:18].clone())   # color
