36492d39fa21868dbc23de30bcbbf21908345d74
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-08-01 10:58:00 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -399,7 +399,7 @@ def hungarian_loss_inclusive_KL(pred, target, loss_fn=F.smooth_l1_loss):
               aux_dist = torch.distributions.Normal(pred[:, :, i:k], torch.tensor(0.01))
               log_prob = -aux_dist.log_prob(target[:, o, i:k].unsqueeze(-2).expand(-1, pred.size(1), -1)).mean(-1)
             elif var == "mask":
-              aux_dist = torch.distributions.Bernoulli(pred[:, :, i:k])
+              aux_dist = torch.distributions.Bernoulli(pred[:, :, i:k].squeeze(-1))
               log_prob = -aux_dist.log_prob(target[:, o, i:k].expand(-1, pred.size(1)))
             else:
               aux_dist = torch.distributions.Categorical(pred[:, :, i:k])
