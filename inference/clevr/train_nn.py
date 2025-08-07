ada4464aefce5ade87ea72ae5154659fb4c0da25
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-08-07 08:01:50 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -1135,7 +1135,9 @@ elif params["running_type"] == "eval":
                           logger.info(pred_coords.tolist())
                           
                           # check, in preds, where 'col_ind[o_idx]' is
-                          pred_abs_idx = pred_coords.tolist().index(real_pred_coords[col_ind[o_idx]].double().tolist())
+                          pred_abs_idx = (pred_coords == real_pred_coords[col_ind[o_idx]]).nonzero(as_tuple=False)
+                          logger.info(pred_abs_idx)
+                          
                           logger.info(f"target index {o} in position {o_idx} -> pred object {col_ind[o_idx]} with abs index {pred_abs_idx}...")
 
                           color_pred = torch.argmax(preds[pred_abs_idx, 10:18], dim=-1).item()
