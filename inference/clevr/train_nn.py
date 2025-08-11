abc43131fb0e6049a9940e00e674862b2a859032
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-08-11 16:40:37 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -871,15 +871,15 @@ elif params["running_type"] == "eval":
         if name == 'image':
           output_images = site["fn"].mean
           D = output_images.size(-1)*output_images.size(-2)
-          sigma = torch.sqrt(((output_images-img)**2).sum(dim=(-1, -2, -3)) / D)
-          sigma = sigma[:, None, None, None]  # [D, 1, 1, 1]
-          sigma = sigma.expand(-1, 3, 128, 128)        
+          sigma = torch.mean(torch.sqrt(((output_images-img)**2).sum(dim=(-1, -2, -3)) / D))
+          # sigma = sigma[:, None, None, None]  # [D, 1, 1, 1]
+          # sigma = sigma.expand(-1, 3, 128, 128)        
           
           logger.info(output_images.shape)
           logger.info(img.shape)
           logger.info(sigma.shape)
           
-          log_wts = pyro.distributions.Normal(output_images, sigma).log_prob(img)
+          log_wts = dist.Independent(dist.Normal(output_images, sigma), params["num_inference_particles"]).log_prob(img)
 
           logger.info(log_wts.shape)
           logger.info(log_wts)
