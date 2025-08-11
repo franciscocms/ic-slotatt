6e10f37abe068042915aeb6692b866e2b8825c55
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-08-11 16:14:24 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -871,7 +871,7 @@ elif params["running_type"] == "eval":
         if name == 'image':
           output_images = site["fn"].mean
           D = output_images.size(-1)*output_images.size(-2)
-          sigma = torch.sqrt(torch.square(output_images-img) / D)          
+          sigma = torch.sqrt(((output_images-img)**2).sum(-1, -2, -3) / D)          
           log_wts = pyro.distributions.Normal(output_images, sigma).log_prob(img)
       
       #log_wts = posterior.log_weights[0]
@@ -1100,8 +1100,7 @@ elif params["running_type"] == "eval":
       trace_slots = trace_slots[batch_idx, indices[:, 1]]
 
       slots_dim = trace_slots.shape[-1]
-      sigma = torch.sqrt(((trace_slots - target_slots)**2).sum(-1, -2, -3) / slots_dim)
-      
+      sigma = torch.sqrt(((trace_slots - target_slots)**2).sum(-1, -2, -3) / slots_dim)     
       log_wts = dist.Normal(trace_slots, torch.tensor(sigma)).log_prob(torch.tensor(target_slots))
       #log_wts = torch.sum(log_wts, dim=(-1, -2))
       resampling = Empirical(torch.stack([torch.tensor(i) for i in range(len(log_wts))]), log_wts)
