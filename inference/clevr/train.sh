671bcde403f47199d433164f02d6a32b8cc6470c
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-08-12 15:40:28 +0100

    Initial commit

--- inference/clevr/train.sh
+++ inference/clevr/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min8gb   # Debug partition
-#SBATCH --qos=gpu_min8gb         # Debug QoS level
+#SBATCH --partition=gpu_min11gb   # Debug partition
+#SBATCH --qos=gpu_min11gb         # Debug QoS level
 #SBATCH --job-name=icsall          # Job name
 #SBATCH -o slurm.%N.%j.out       # File containing STDOUT output
 #SBATCH -e slurm.%N.%j.err       # File containing STDERR output
