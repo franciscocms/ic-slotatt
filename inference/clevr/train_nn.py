e5da6f70da6aa47019957f7492b64812c25c9ba1
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-09-13 08:45:50 +0100

    Initial commit

--- inference/clevr/train_nn.py
+++ inference/clevr/train_nn.py
@@ -512,10 +512,14 @@ def hungarian_loss_inclusive_KL(pred, target, loss_fn=F.smooth_l1_loss):
                
             #logger.info(f"var {var} - log_prob using pred with shape {pred[:, :, i:k].shape} for {i} to {k}")
 
-            logger.info(f"var {var} - {log_prob.shape}")
+            #
                                         
             i = k
-            log_prob = log_prob.unsqueeze(-1)
+            if len(log_prob.shape) < 3:
+              log_prob = log_prob.unsqueeze(-1)
+            
+            logger.info(f"var {var} - {log_prob.shape}")
+            
             latent_pdist = torch.cat((latent_pdist, log_prob), dim=-1) # [B, N, nlatents]
 
         latent_pdist = latent_pdist.unsqueeze(-2) # [B, N, 1, nlatents]
