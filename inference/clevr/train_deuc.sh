3cc77599d4ffe5eaf1876b97928844874de1870d
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-11-11 17:43:01 +0000

    Initial commit

--- inference/clevr/train_deuc.sh
+++ inference/clevr/train_deuc.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#
-#SBATCH --partition=gpu_min32gb   # Debug partition
-#SBATCH --qos=gpu_min32gb         # Debug QoS level
-#SBATCH --job-name=103            # Job name
-#SBATCH -o slurm.%N.%j.out       # File containing STDOUT output
-#SBATCH -e slurm.%N.%j.err       # File containing STDERR output
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=5:00:00
+#SBATCH --partition normal-a100-40
+#SBATCH --gpus=1
+#SBATCH --account=f202500016aivlabdeucaliong ##should end in G
 
-python train_nn.py
+
+ml  OpenMPI/5.0.3-GCC-13.3.0 CUDA/11.8.0 NCCL/2.20.5-GCCcore-13.3.0-CUDA-12.4.0
+
+srun train
