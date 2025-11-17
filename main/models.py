6e23920defdb2877ab098b5a7444bccea673504f
Francisco Carvalho Silva <francisco.c.silva@inesctec.pt>
2025-11-17 12:46:50 +0000

    Initial commit

--- main/models.py
+++ main/models.py
@@ -158,16 +158,18 @@ def sample_scenes():
 
 def model(observations={"image": torch.zeros((1, 3, 128, 128))}):
 
-  init_time = time.time()
+  #init_time = time.time()
   
   B = params['batch_size'] if params["running_type"] == "train" else params['num_inference_samples']
   
   scenes = sample_scenes()
   rendered_scenes = render(scenes)
   img = torch.stack([img_transform(s) for s in rendered_scenes])
+
+  logger.info(img.shape)
   
-  render_time = time.time() - init_time
-  logger.info(f"Batch generation duration: {render_time} - {render_time/B} per sample")
+  #render_time = time.time() - init_time
+  #logger.info(f"Batch generation duration: {render_time} - {render_time/B} per sample")
 
   llh_uncertainty = 0.001 if params['running_type'] == "train" else 0.05
   likelihood_fn = MyNormal(img, torch.tensor(llh_uncertainty)).get_dist()
